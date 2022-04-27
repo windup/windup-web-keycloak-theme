45972c03bb3623d8727d02fd527a1238ac7cec73
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2022-04-27 15:10:55 +0200

    Change folders from theme to themes

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -12,14 +12,14 @@ xmlstarlet ed \
 -u '/s:server/s:profile/su:subsystem/su:theme/su:staticMaxAge' -v "-1" \
 -u '/s:server/s:profile/su:subsystem/su:theme/su:cacheThemes' -v "false" \
 -u '/s:server/s:profile/su:subsystem/su:theme/su:cacheTemplates' -v "false" \
--u '/s:server/s:profile/su:subsystem/su:theme/su:dir' -v "$(pwd)/src/main/resources/theme" \
+-u '/s:server/s:profile/su:subsystem/su:theme/su:dir' -v "$(pwd)/src/main/resources/themes" \
 workspace/keycloak-*/standalone/configuration/standalone.xml
 
-cp -r workspace/keycloak-*/themes/* src/main/resources/theme/
+cp -r workspace/keycloak-*/themes/* src/main/resources/themes/
 
-cp src/main/resources/theme/windup/login/login_required.theme.properties src/main/resources/theme/windup/login/theme.properties
-cp src/main/resources/theme/mta/login/login_required.theme.properties src/main/resources/theme/mta/login/theme.properties
-cp src/main/resources/theme/tackle/login/login_required.theme.properties src/main/resources/theme/tackle/login/theme.properties
-cp src/main/resources/theme/mtr/login/login_required.theme.properties src/main/resources/theme/mtr/login/theme.properties
+cp src/main/resources/themes/windup/login/login_required.theme.properties src/main/resources/themes/windup/login/theme.properties
+cp src/main/resources/themes/mta/login/login_required.theme.properties src/main/resources/themes/mta/login/theme.properties
+cp src/main/resources/themes/tackle/login/login_required.theme.properties src/main/resources/themes/tackle/login/theme.properties
+cp src/main/resources/themes/mtr/login/login_required.theme.properties src/main/resources/themes/mtr/login/theme.properties
 
 echo "You can start the server using ./workspace/keycloak-$KcVersion/bin/standalone.sh"
